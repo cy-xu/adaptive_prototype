216483e7207f4f1181ae87ecaa8cc218fd6ac9c5
CY <hello.xu@gmail.com>
2023-03-10 13:20:45 -0800

    fiftyone

---
+++ view_coco_dataset.py
@@ -0,0 +1,14 @@
+import fiftyone
+
+dataset = fiftyone.zoo.load_zoo_dataset("coco-2017")
+
+dataset = fiftyone.zoo.load_zoo_dataset(
+    "coco-2017",
+    split="validation",
+    label_types=["detections", "segmentations"],
+    classes=["person", "car"],
+    max_samples=50,
+)
+
+# Visualize the dataset in the FiftyOne App
+session = fiftyone.launch_app(dataset)
